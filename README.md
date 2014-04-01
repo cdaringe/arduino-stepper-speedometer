e58e089db3b44054a3f6fb0e988d7acdad6e37c3
Christoher Dieringer <cdieringer@mrn.org>
2014-04-01 00:40:14 -0700

    enhanced to handle speed1

--- README.md
+++ README.md
@@ -10,3 +10,8 @@
 * Adjust the #defines to match your speedometer hardware
 * Upload
 * Go!
+
+##Notes
+* This implementation does NOT have you program in speeds/etc, but rather, the angle span of the dial.  It assumes your max speed is at the max angle of the speedometer.  As current dial angle is to max dial angle, current speed is to max speed.  Interpolate.
+* A speedometer may be better suited for a servo motor, over a stepper, given the ability to map pulses to a fixed motor angle
+* This implementation does not home the dial on startup.  Thus, if there is a power loss during activity, the user must reset the dial to its 0 position
--- speed.ino
+++ speed.ino
@@ -8,30 +8,36 @@ int sensepin = 2;
 int dirpin = 11;
 int steppin = 12;
 
-#define MAXSPEED 180
-#define MINSPEED 0
-#define PULSECOUNTMAXSPEED (8) //number of pulses to map to max speed
-#define SPEEDBUFFSIZE 10
+#define MAXSPEED 9000 //mm/s (9000mm/s ~20mph)
+#define MAXSPEEDANGLE 180 //max range of the dial
+#define MINSPEEDANGLE 0 //Keep at 0
+#define STEPSPERREV (200*8) //consult your motor and stepper driver board.  Scale to compensate for microsteps.  Easy driver with no addons defaults to 8 microsteps/step
+#define WHEELDIA 25.4 //mm
+#define NUMSENSORS 1 //i.e., if you may have 2 wheel magnets on a magnetic wheel sensor
+const double DISTANCE_PER_PULSE = WHEELDIA*3.1415926/NUMSENSORS; //mm
+#define SPEEDBUFFSIZE 10 //smoothing constant. larger:less-responsive, smaller:more-responsive. Recommend 3-20
+#define T_INTERVAL 100 // (milliseconds) time duration for each capturing period.  Recommend as low as possible, so long as you can get substantial pulses within that duration
+// NOTE : It will take SPEEDBUFFSIZE * T_INVERVAL time for the speedometer to return to 0 after the object's motion has stopped
+#define MOTORDELAYMICRO 200 //delay between pulses for motor (microseconds).  You should make this as small as feasible, with a ~30-50 safety time added.  Start at 200, by 10's/20's up or down
+const double PULSESTOMAXSPEED = (MAXSPEED*(T_INTERVAL/1000))/DISTANCE_PER_PULSE;
 
-#define MOTORDELAYMICRO 200 //delay between pulses for motor.  You should make this as small as possible.  Start here
-
-double interval_end;
+double interval_end;  //marks the end of a T_INTERVAL period of data collection
 int speedCounts[SPEEDBUFFSIZE]; //array of speed/pulse counts
-int interval_t = 100; //ms
-
 int currentStep = 0;
 
 
 
 void setup() {
   Serial.begin(38400);
-  Serial.println("Go go gadget speedometer!");
+  Serial.println("Speedometer logging started...");
+  Serial.print("Pulses to max speed: ");
+  Serial.println(PULSESTOMAXSPEED);
   pinMode(sensepin, INPUT);
   digitalWrite(sensepin,HIGH);
   pinMode(dirpin, OUTPUT);
   pinMode(steppin, OUTPUT);
   attachInterrupt(0, pulseSense, FALLING);
-  interval_end = millis() + interval_t;
+  interval_end = millis() + T_INTERVAL;
 }
 
 
@@ -40,17 +46,21 @@ void pulseSense() {
   ++pulseCount;  
 }
 
+
+
 void loop() {
   if (millis() > interval_end) {
     shiftArray(speedCounts,SPEEDBUFFSIZE);
     speedCounts[0] = pulseCount;
-    pulseCount = 0; //reset counter
-    interval_end += interval_t;
+    pulseCount = 0;
+    interval_end += T_INTERVAL;
     moveDial();
   }
 }
 
-//shifts all elements to the right
+
+
+//shifts all array elements to the right
 //element of array[0] set to 0
 void shiftArray(int* arr, int arrSize) {
   for (int k = arrSize-1; k > 0; k--) 
@@ -61,17 +71,27 @@ void shiftArray(int* arr, int arrSize) {
   return;
 }
 
-void moveDial() {
-   //convert speed to position on dial
 
-   //int destStep = map(speedCounts[0],0,PULSECOUNTMAXSPEED,MINSPEED,MAXSPEED);
-   double approxDestStep =0;
+/**
+ * convert speed data to dial position, and command dial to that position
+ */
+void moveDial() {
+
+   double approxDestStep = 0;
+   double currSpeed;
    for (int i = 0; i<SPEEDBUFFSIZE; ++i) {
-     approxDestStep += map(speedCounts[i],0,PULSECOUNTMAXSPEED,MINSPEED,MAXSPEED)/SPEEDBUFFSIZE; //creates average of past speeds
+     currSpeed = speedCounts[i]/PULSESTOMAXSPEED;
+     if (currSpeed >= MAXSPEED) currSpeed = MAXSPEED;
+     approxDestStep += map(currSpeed, 
+                            0, 
+                            MAXSPEED, 
+                            MINSPEEDANGLE, 
+                            MAXSPEEDANGLE)
+                            /SPEEDBUFFSIZE; //creates average of past speeds
    }
    int destStep = (int) approxDestStep;
-      
-
+  
+   //Uncomment below to monitor your motion vitals
    //Serial.print("curr: "); Serial.print(currentStep);Serial.print(" // dest: "); Serial.print(destStep);Serial.print(" // pulses: "); Serial.println(speedCounts[0]);
    
     //set direction of move
